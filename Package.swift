2f272ea50eb36f0bbd22d260f70fe38d68f49bb5
emawby <emawby@gmail.com>
2021-05-20 14:35:30 -0700

    testing dynamic

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "OneSignal",
-            tpye: .static,
+            type: .dynamic,
             targets: ["OneSignal"]),
     ],
     targets: [
