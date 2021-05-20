5eb382e64022031e7106368b32b90859b4bf49b5
emawby <emawby@gmail.com>
2021-05-19 19:28:17 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,12 +8,13 @@ let package = Package(
     products: [
         .library(
             name: "OneSignal",
+            tpye: .static,
             targets: ["OneSignal"]),
     ],
     targets: [
         .binaryTarget(
-          name: "OneSignal",
           url: "https://github.com/OneSignal/OneSignal-XCFramework/releases/download/test/OneSignal.xcframework.zip",
+          checksum: "3f229fbcd336a908383da2c265e6d797aa0cf9c5a09c67aa246a42b224e05106"
           checksum: "3f229fbcd336a908383da2c265e6d797aa0cf9c5a09c67aa246a42b224e05106"
         )
     ]
