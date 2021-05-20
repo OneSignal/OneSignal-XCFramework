8064e7f918ee9afcfb84ea4435ed2411cc13df23
emawby <emawby@gmail.com>
2021-05-20 14:45:21 -0700

    trying 5.4 swift tools

--- Package.swift
+++ Package.swift
@@ -14,6 +14,5 @@ let package = Package(
         .binaryTarget(
           url: "https://github.com/OneSignal/OneSignal-XCFramework/releases/download/test/OneSignal.xcframework.zip",
           checksum: "3f229fbcd336a908383da2c265e6d797aa0cf9c5a09c67aa246a42b224e05106"
-        )
     ]
 )
