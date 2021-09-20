a75ad400e3c1828ecf9860482b17f68f6de21535
emawby <emawby@gmail.com>
2021-09-20 16:02:02 -0700

    local package using path

--- Package.swift
+++ Package.swift
@@ -5,16 +5,5 @@ import PackageDescription
 
 let package = Package(
     name: "OneSignalXCFramework",
-    products: [
-        .library(
-            name: "OneSignal",
-            targets: ["OneSignal"]),
-    ],
-    targets: [
-        .binaryTarget(
-          name: "OneSignal",
-          url: "https://github.com/OneSignal/OneSignal-iOS-SDK/releases/download/3.8.0/OneSignal.xcframework.zip",
-          checksum: "547fee80d9263fc1bf5484c9a311e582d4df23941f7ff16261af427dd8f0442e"
-        )
-    ]
+    path: "OneSignal.xcframework"
 )
