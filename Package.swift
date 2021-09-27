98113618bdf4242440ea9ae7366a3a7f4390282a
emawby <emawby@gmail.com>
2021-09-27 14:00:31 -0700

    proper package setup

--- Package.swift
+++ Package.swift
@@ -4,27 +4,57 @@
 import PackageDescription
 
 let package = Package(
-    name: "OneSignalFinalLib",
+    name: "OneSignal",
     products: [
         .library(
-            name: "OneSignalFinalLib",
-            targets: ["OneSignalFinalLib"]),
+            name: "OneSignal",
+            targets: ["OneSignalWrapper"]),
+        .library(
+            name: "OneSignalExtension",
+            targets: ["OneSignalExtensionWrapper"]),
+            .library(
+            name: "OneSignalLocation",
+            targets: ["OneSignalLocationWrapper"]),
     ],
     targets: [
         .binaryTarget(
-          name: "OneSignalSharedTest",
+          name: "OneSignal",
           path: "OneSignal.xcframework"
         ),
         .binaryTarget(
-          name: "OneSignalSwiftTest",
-          path: "OneSignalSwift.xcframework"
+          name: "OneSignalCore",
+          path: "OneSignalCore.xcframework"
         ),
-        .target(name: "OneSignalFinalLib",
+        .binaryTarget(
+          name: "OneSignalLocation",
+          path: "OneSignalLocation.xcframework"
+        ),
+        .binaryTarget(
+          name: "OneSignalExtension",
+          path: "OneSignalExtension.xcframework"
+        ),
+        .target(name: "OneSignalWrapper",
                 dependencies: [
-                    .target(name: "OneSignalSharedTest"),
-                    .target(name: "OneSignalSwiftTest")
+                    .target(name: "OneSignal"),
+                    .target(name: "OneSignalCore")
+                    .target(name: "OneSignalExtension")
                 ],
-                path: "OneSignalFinal"
+                path: "OneSignalWrapper"
+        )
+        .target(name: "OneSignalExtensionWrapper",
+                dependencies: [
+                    .target(name: "OneSignalCore")
+                    .target(name: "OneSignalExtension")
+                ],
+                path: "OneSignalExtensionWrapper"
+        )
+        .target(name: "OneSignalLocationWrapper",
+                dependencies: [
+                    .target(name: "OneSignalCore")
+                    .target(name: "OneSignalLocation")
+                ],
+                path: "OneSignalLocationWrapper"
         )
     ]
 )
+
