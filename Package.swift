6de3072473019f6828d82a5f56a7f9e4d49db41b
emawby <emawby@gmail.com>
2022-03-22 14:46:54 -0700

    Release 3.10.2

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
           name: "OneSignal",
-          url: "https://github.com/OneSignal/OneSignal-iOS-SDK/releases/download/3.10.1/OneSignal.xcframework.zip",
-          checksum: "cd2977f8f94ba6d5763746911d10b0633ff577508d18e79fe5eaf76f326a8324"
+          url: "https://github.com/OneSignal/OneSignal-iOS-SDK/releases/download/3.10.2/OneSignal.xcframework.zip",
+          checksum: "b17aca2cb03bac87be5a219903eedda2ce3430c99949ce340fc00450b37c0a67"
         )
     ]
 )
